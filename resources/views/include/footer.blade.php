1b1e8599cd5f0aba107563b7d4fabe72592fef4e
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-04-04 01:57:59 +0300

    commit init

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,22 @@
+<!-- Start Footer Section -->
+<section id="footer" class="bg-dark">
+        <div class="footer">
+            <h1>Yemen UP</h1>
+            <div class="icons">
+                <img src="{{ URL::asset('images/icon.png')}}" alt="" srcset="">
+                <img src="{{ URL::asset('images/icon1.png')}}" alt="" srcset="">
+                <img src="{{ URL::asset('images/icon2.png')}}" alt="" srcset="">
+                <img src="{{ URL::asset('images/icon3.png')}}" alt="" srcset="">
+            </div>
+            <p>Copyright &copy YemenUP</p>
+        </div>
+    </section>
+    <!-- End Footer Section -->
+
+    <script src="{{ URL::asset('js/jquery-3.5.1.min.js')}}"></script>
+    <script src="{{ URL::asset('js/jquery-ui.min.js')}}"></script>
+    <script src="{{ URL::asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{ URL::asset('js/main.js')}}"></script>
+
+</body>
+</html>
---
+++ resources/views/include/nav.blade.php
@@ -0,0 +1,50 @@
+<div class="upper-bar">
+        <div class="container">
+            <div class="navbar navbar-expand-lg session-user" id="sessionUser">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0" >
+                    <img src="img.png" class="img-thumbnail img-circle" alt="" />
+                    <li class="nav-item dropdown">
+                        <a class="btn btn-default  nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="include/logout.html">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <a href="include/login.html">
+                <span class="login-header pull-right" id="login-header">Login/Signup</span>
+            </a>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">YemenUp</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="include/jobs.html">All Jobs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="companies">All Comps</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="include/about.html">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/include/contact.html">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
